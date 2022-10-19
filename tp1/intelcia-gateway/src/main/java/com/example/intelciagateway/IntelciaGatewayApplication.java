fa509fd4c54be697bbfe1e2de6d6a1711a01880a
AITSIHAMZA <hamzaaitsi65@gmail.com>
2022-10-19 15:19:07 +0100

    feat:spring cloud gateway part1

---
+++ tp1/intelcia-gateway/src/main/java/com/example/intelciagateway/IntelciaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.intelciagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelciaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelciaGatewayApplication.class, args);
+    }
+
+}
---
+++ tp1/intelcia-gateway/src/test/java/com/example/intelciagateway/IntelciaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intelciagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelciaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
